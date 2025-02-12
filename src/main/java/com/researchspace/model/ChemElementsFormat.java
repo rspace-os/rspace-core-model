66fa8ada16567e8d80430f344997c253b332b4d0
rs-fraser <fraser.macinnes>
2025-02-12 08:19:38 +0000

    add new chemical formats

--- src/main/java/com/researchspace/model/ChemElementsFormat.java
+++ src/main/java/com/researchspace/model/ChemElementsFormat.java
@@ -3,23 +3,11 @@ package com.researchspace.model;
  * The format that the 'chemElements' column is stored in.
  */
 public enum ChemElementsFormat {
-	
-	
-	
-	/**
-	 * Mol format
-	 */
 	MOL("mol"),
-	
-	/**
-	 * Marvin XML format
-	 */
 	MRV("mrv"),
-	
-	/**
-	 * Ketcher format
-	 */
-	KET("ket");
+	KET("ket"),
+	CDXML("cdxml"),
+	SMI("smi");
 	
 	public String getLabel() {
 		return label;
