70a8db5862e7a491e5411930ce5c188cd46f5cf0
Nico Ferrante <nicola.ferrante@researchspace.com>
2025-05-07 15:08:18 +0100

    PRT-913: Extending SharingResult object to contain error list

--- src/main/java/com/researchspace/model/dto/SharingResult.java
+++ src/main/java/com/researchspace/model/dto/SharingResult.java
@@ -1,5 +1,6 @@
 package com.researchspace.model.dto;
 
+import com.researchspace.model.field.ErrorList;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,4 +12,5 @@ import java.util.List;
 public class SharingResult {
     private List<Long> sharedIds;
     private List<String> publicLinks;
+    private ErrorList error;
 }
