8a5d0fb3f1c7d55042ae461e728c38cb3ca6babe
Fraser <141634676+rs-fraser@users.noreply.github.com>
2025-08-07 13:17:51 +0100

    add stoichiometry models

---
+++ src/main/java/com/researchspace/model/stoichiometry/Stoichiometry.java
@@ -0,0 +1,52 @@
+package com.researchspace.model.stoichiometry;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.researchspace.model.RSChemElement;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "Stoichiometry")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@ToString(exclude = "molecules")
+public class Stoichiometry {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "parent_reaction_id", nullable = false)
+  private RSChemElement parentReaction;
+
+  @Builder.Default
+  @OneToMany(
+          mappedBy = "stoichiometry",
+          cascade = CascadeType.ALL,
+          fetch = javax.persistence.FetchType.EAGER)
+  private List<StoichiometryMolecule> molecules = new ArrayList<>();
+
+  public void addMolecule(StoichiometryMolecule molecule) {
+    molecules.add(molecule);
+    molecule.setStoichiometry(this);
+  }
+}
