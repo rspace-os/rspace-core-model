b0fd4e220e6f30d454cd5b2d73104fdbb5cddd9e
matthias <matthias@researchspace.com>
2026-03-19 16:50:53 +0000

    add NfsAuthenticationType.SERVER enum value, and clear unused enum method

--- src/main/java/com/researchspace/model/netfiles/NfsAuthenticationType.java
+++ src/main/java/com/researchspace/model/netfiles/NfsAuthenticationType.java
@@ -13,19 +13,11 @@ public enum NfsAuthenticationType {
 	/**
 	 * public/private key authentication
 	 */
-	PUBKEY;
-	
-	/**
-	 * Get enum object for linked string value
-	 */
-	public static NfsAuthenticationType fromString(String authTypeString) {
-		if ("password".equals(authTypeString)) {
-			return PASSWORD;
-		}
-		if ("pubKey".equals(authTypeString)) {
-			return PUBKEY;
-		}
-		throw new IllegalArgumentException("unrecognised authentication type: " + authTypeString);
-	}
+	PUBKEY,
 
+	/**
+	 * if server-configured authentication should be used
+	 */
+	SERVER
+	
 }
